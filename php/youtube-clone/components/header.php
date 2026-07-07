8e7aff98c55c04edc26efad94df2a96e884a7aa3
joy1668 <69751471+joy1668@users.noreply.github.com>
2026-07-07 20:56:22 +0800

    Upload php/youtube-clone/components/header.php

---
+++ php/youtube-clone/components/header.php
@@ -0,0 +1,54 @@
+<header class="header">
+    <div class="header-left">
+        <button class="icon-btn menu-btn" aria-label="Menu">
+            <svg viewBox="0 0 24 24" width="24" height="24" fill="currentColor">
+                <path d="M3 6h18v2H3V6zm0 5h18v2H3v-2zm0 5h18v2H3v-2z"/>
+            </svg>
+        </button>
+        <a href="/" class="logo">
+            <svg viewBox="0 0 90 20" width="90" height="20" fill="currentColor">
+                <g>
+                    <path d="M27.97 4.12v11.62c0 1.24-1.01 2.25-2.25 2.25s-2.25-1.01-2.25-2.25V4.12h-2.08v11.62c0 2.39 1.94 4.33 4.33 4.33s4.33-1.94 4.33-4.33V4.12h-2.08z" fill="#FF0000"/>
+                    <path d="M46.72 10.57c0-2.02-1.01-3.1-2.47-3.1-1.46 0-2.47 1.08-2.47 3.1v.87c0 2.02 1.01 3.1 2.47 3.1 1.46 0 2.47-1.08 2.47-3.1v-.87zm-7.02 5.17h2.08V7.31h-2.08v1.31c-.51-.93-1.38-1.48-2.68-1.48-2.15 0-3.79 1.74-3.79 4.28v.89c0 2.5 1.63 4.27 3.79 4.27 1.3 0 2.17-.55 2.68-1.48v1.34z"/>
+                    <path d="M57.18 5.03h-4.77v10.72h2.08v-3.94h2.69c1.98 0 3.28-1.27 3.28-3.39v-.01c0-2.11-1.3-3.38-3.28-3.38zm.18 4.94h-2.87V6.66h2.87c.91 0 1.47.59 1.47 1.65 0 1.07-.56 1.66-1.47 1.66z"/>
+                    <path d="M68.83 5.03h-2.08v3.77h-3.08V5.03h-2.08v10.72h2.08v-5.32h3.08v5.32h2.08V5.03z"/>
+                    <path d="M75.88 8.59c-.66-.38-1.2-.56-1.2-1.14 0-.52.42-.84 1.09-.84.66 0 1.54.32 2.15.81l1.08-1.38c-.77-.72-1.89-1.08-3.15-1.08-1.86 0-3.17 1.18-3.17 2.66 0 1.57 1.04 2.15 2.23 2.71.75.37 1.33.57 1.33 1.18 0 .58-.5.93-1.27.93-.82 0-1.82-.41-2.5-.98l-1.14 1.38c.92.83 2.14 1.25 3.55 1.25 1.98 0 3.37-1.16 3.37-2.77 0-1.65-1.15-2.26-2.37-2.73z"/>
+                    <path d="M87.35 5.03l-3.08 7.55-3.07-7.55h-2.27l4.12 9.63c-.06.28-.34.52-.7.52-.12 0-.21 0-.3-.03v1.73c.1.01.22.02.35.02 1.15 0 1.96-.52 2.61-2.05l4.59-9.82h-2.25z"/>
+                </g>
+            </svg>
+        </a>
+    </div>
+
+    <div class="header-center">
+        <div class="search-bar">
+            <input type="text" class="search-input" placeholder="Search" />
+            <button class="search-btn" aria-label="Search">
+                <svg viewBox="0 0 24 24" width="24" height="24" fill="currentColor">
+                    <path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0016 9.5 6.5 6.5 0 109.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/>
+                </svg>
+            </button>
+        </div>
+        <button class="icon-btn mic-btn" aria-label="Search with your voice">
+            <svg viewBox="0 0 24 24" width="24" height="24" fill="currentColor">
+                <path d="M12 14c1.66 0 3-1.34 3-3V5c0-1.66-1.34-3-3-3S9 3.34 9 5v6c0 1.66 1.34 3 3 3z"/>
+                <path d="M17 11c0 2.76-2.24 5-5 5s-5-2.24-5-5H5c0 3.53 2.61 6.43 6 6.92V21h2v-3.08c3.39-.49 6-3.39 6-6.92h-2z"/>
+            </svg>
+        </button>
+    </div>
+
+    <div class="header-right">
+        <button class="icon-btn" aria-label="Create">
+            <svg viewBox="0 0 24 24" width="24" height="24" fill="currentColor">
+                <path d="M14 13h-3v3H9v-3H6v-2h3V8h2v3h3v2zm3-7H3v12h14v-6.39l4 1.83V8.56l-4 1.83V6m1-2v3.83L22 7v8l-4-1.83V19H2V4h16z"/>
+            </svg>
+        </button>
+        <button class="icon-btn" aria-label="Notifications">
+            <svg viewBox="0 0 24 24" width="24" height="24" fill="currentColor">
+                <path d="M12 22c1.1 0 2-.9 2-2h-4c0 1.1.9 2 2 2zm6-6v-5c0-3.07-1.64-5.64-4.5-6.32V4c0-.83-.67-1.5-1.5-1.5s-1.5.67-1.5 1.5v.68C7.63 5.36 6 7.92 6 11v5l-2 2v1h16v-1l-2-2z"/>
+            </svg>
+        </button>
+        <button class="avatar-btn" aria-label="Your channel">
+            <span class="avatar" style="background: #FF5722;">C</span>
+        </button>
+    </div>
+</header>
